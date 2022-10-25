784aa590634b3584a78c2bbe0332dda2f29732eb
Guoyutian <929458988@qq.com>
2022-10-25 15:42:34 +0800

    lc540

---
+++ src/main/java/bs/lc540/Test540.java
@@ -0,0 +1,22 @@
+package bs.lc540;
+import bs.lc744.Lc744;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class Test540 {
+
+    private static Lc540 lc540;
+
+    @BeforeAll
+    static void initLc215(){
+        lc540 = new Lc540();
+    }
+
+    @Test
+    void checkResult(){
+        int k = 5;
+        int[] nums = new int[] {1, 1, 2, 2, 3, 3, 4, 5, 5};
+        int res = lc540.singleNonDuplicate(nums);
+        System.out.println(res);
+    }
+}
