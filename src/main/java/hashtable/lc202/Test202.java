d8c32178539d51aa125277346cd878ea0202dd7b
Guoyutian <929458988@qq.com>
2022-10-26 15:34:37 +0800

    lc202

---
+++ src/main/java/hashtable/lc202/Test202.java
@@ -0,0 +1,22 @@
+package hashtable.lc202;
+
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class Test202 {
+    private static Lc202 lc202;
+
+    @BeforeAll
+    static void initLc202(){
+        lc202 = new Lc202();
+    }
+
+    @Test
+    void checkResult(){
+        int k = 55;
+        boolean res = lc202.isHappy(k);
+        System.out.println(res);
+    }
+}
