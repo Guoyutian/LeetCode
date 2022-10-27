7d02c55463bc3b2814de202807a5d5ad4046060c
Guoyutian <929458988@qq.com>
2022-10-27 15:43:46 +0800

    sw2

---
+++ src/main/java/sw/lc187/Lc187.java
@@ -0,0 +1,31 @@
+package sw.lc187;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Lc187 {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        if (s == null || s.length() < 10) return result;
+        int start = 0;
+        int end = 0;
+        Map<String, Integer> map = new HashMap<>();
+
+        while (end < s.length()) {
+            if (end - start + 1 < 10) {
+                end++;
+            } else if (end - start + 1 == 10) {
+                String st = s.substring(start, end+1);
+                map.put(st, map.getOrDefault(st, 0)+1);
+                if (map.get(st) == 2) {
+                    result.add(st);
+                }
+                end++;
+                start++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/sw/lc187/Test187.java
@@ -0,0 +1,23 @@
+package sw.lc187;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import sw.lc438.Lc438;
+
+import java.util.List;
+
+public class Test187 {
+    private static Lc187 lc187;
+
+    @BeforeAll
+    static void initLc438(){
+        lc187 = new Lc187();
+    }
+
+    @Test
+    void checkResult(){
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        List<String> res = lc187.findRepeatedDnaSequences(s);
+        System.out.println(res.toString());
+    }
+}
