56957aaf1031ccc947b24baf51bdfabdf9553f3c
Guoyutian <929458988@qq.com>
2022-11-06 14:31:31 +0800

    lc234

---
+++ src/main/java/linked_list/lc234/Lc234.java
@@ -0,0 +1,38 @@
+package linked_list.lc234;
+
+import linked_list.listnode.ListNode;
+
+public class Lc234 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
