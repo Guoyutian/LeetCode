e4c5548ff658a28edac45fa52e92d05c03f7ffd7
Guoyutian <929458988@qq.com>
2022-11-08 09:26:31 +0800

    lc695

---
+++ src/main/java/search/lc695/Lc695.java
@@ -0,0 +1,78 @@
+package search.lc695;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Lc695 {
+    private static int[][] DIRECTIONS = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    // BFS
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int M = grid.length;
+        int N = grid[0].length;
+        boolean[][] visited = new boolean[M][N];
+        int res = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    res = Math.max(res, bfs(grid, visited, i, j));
+                }
+            }
+        }
+        return res;
+    }
+
+
+    private int bfs(int[][] grid, boolean[][] visited, int i, int j) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
+        visited[i][j] = true;
+        int res = 0;
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            res++;
+            for (int[] dir : DIRECTIONS) {
+                int x = curr[0] + dir[0];
+                int y = curr[1] + dir[1];
+                if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || visited[x][y] || grid[x][y] != 1)
+                    continue;
+                q.add(new int[]{x, y});
+                visited[x][y] = true;
+            }
+        }
+        return res;
+    }
+
+
+    // DFS
+    public int maxAreaOfIsland2(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int M = grid.length;
+        int N = grid[0].length;
+        boolean[][] visited = new boolean[M][N];
+        int res = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    res = Math.max(res, dfs(grid, visited, i, j));
+                }
+            }
+        }
+        return res;
+    }
+
+    private int dfs(int[][] grid, boolean[][] visited, int i, int j) {
+        if (i < 0 || j < 0 ||
+                i >= grid.length || j >= grid[0].length ||
+                grid[i][j] != 1 || visited[i][j]) return 0;
+        visited[i][j] = true;
+        int res = 1;
+        for (int[] dir : DIRECTIONS) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            res += dfs(grid, visited, x, y);
+        }
+        return res;
+    }
+}
