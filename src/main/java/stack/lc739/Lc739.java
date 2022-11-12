f5468cacb2f1c16be05656a01bab1dddf320af84
Guoyutian <929458988@qq.com>
2022-11-12 11:51:44 +0800

    stack

---
+++ src/main/java/stack/lc739/Lc739.java
@@ -0,0 +1,18 @@
+package stack.lc739;
+
+import java.util.Stack;
+
+public class Lc739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ret = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ret[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+}
