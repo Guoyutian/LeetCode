ede6992f72bdc7bcf0ec2c6a33158ab5f551c667
Guoyutian <929458988@qq.com>
2022-11-13 12:58:53 +0800

    back

--- src/main/java/back_tracking/lc40/Lc40.java
+++ src/main/java/back_tracking/lc40/Lc40.java
@@ -1,5 +1,6 @@
 package back_tracking.lc40;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,7 +9,7 @@ public class Lc40 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
         // Sort the candidates as this will help in avoiding duplicate subsets
-        Arrays.sort( candidates );
+        Arrays.sort(candidates);
 
         List<List<Integer>> result = new LinkedList<>();
 
@@ -25,39 +26,60 @@ public class Lc40 {
 
         /* If target has reached 0, current subset is a valid combination,
         add it to the result and backtrack */
-        if( target == 0 ) {
-            result.add( new LinkedList<>( subset ));
+        if (target == 0) {
+            result.add(new LinkedList<>(subset));
             return;
         }
 
         /* If current chain has ended and the target is still not 0
         then no valid subset is present in this path and hence backtrack */
-        if( start == candidates.length ) {
+        if (start == candidates.length) {
             return;
         }
 
         // Generate subsets for current level
-        for(int i = start; i < candidates.length; i++) {
+        for (int i = start; i < candidates.length; i++) {
 
             /* If current number is greater than target and since numbers are sorted,
             no need to generate subset from here as it will exceed the target */
-            if( candidates[i] > target ) {
+            if (candidates[i] > target) {
                 break;
             }
 
             /* If current no. is same as previous number, skip current number to
             avoid duplicate subsets */
-            if( i != start && candidates[i] == candidates[i-1] ) {
+            if (i != start && candidates[i] == candidates[i - 1]) {
                 continue;
             }
 
             // Include current number in the subset and subtract it from the target
-            subset.add( candidates[i] );
-            combinationSum2(candidates, target - candidates[i], i+1, subset, result);
+            subset.add(candidates[i]);
+            combinationSum2(candidates, target - candidates[i], i + 1, subset, result);
 
             /* Remove the current number from subset after all the subsets from
             current number has been generated */
-            subset.remove( subset.size() - 1 );
+            subset.remove(subset.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum_2(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else {
+            for (int i = start; i < nums.length; i++) {
+                if (i > start && nums[i] == nums[i - 1]) continue; // skip duplicates
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
         }
     }
 }
---
+++ src/main/java/linked_list/lc876/Lc876.java
@@ -0,0 +1,14 @@
+package linked_list.lc876;
+
+import linked_list.listnode.ListNode;
+
+public class Lc876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
