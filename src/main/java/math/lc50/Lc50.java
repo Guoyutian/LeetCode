d9accb9ef48f7165844a240b0b2922825526ec4e
Guoyutian <929458988@qq.com>
2022-11-30 18:27:15 +0800

    ts2

---
+++ src/main/java/math/lc50/Lc50.java
@@ -0,0 +1,12 @@
+package math.lc50;
+
+public class Lc50 {
+    public double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n < 0) {
+            return 1 / x * myPow(1 / x, -(n + 1));
+        }
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+}
