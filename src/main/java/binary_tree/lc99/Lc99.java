573bd9145aba02b57d7e302d49a56d126a647749
Guoyutian <929458988@qq.com>
2022-12-06 10:57:01 +0800

    pre3

---
+++ src/main/java/binary_tree/lc99/Lc99.java
@@ -0,0 +1,25 @@
+package binary_tree.lc99;
+
+import binary_tree.treenode.TreeNode;
+
+public class Lc99 {
+    TreeNode prev = null, first = null, second = null;
+
+    public void recoverTree(TreeNode root) {
+        evalSwappedNodes(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void evalSwappedNodes(TreeNode curr) {
+        if (curr == null) return;
+        evalSwappedNodes(curr.left);
+        if (prev != null && prev.val > curr.val) {
+            if (first == null) first = prev;
+            second = curr;
+        }
+        prev = curr;
+        evalSwappedNodes(curr.right);
+    }
+}
