9a50380a14643607f0ede6def54a76f2e1cacda0
Guoyutian <929458988@qq.com>
2022-12-08 11:50:26 +0800

    pre9

---
+++ src/main/java/string/lc434/Lc434.java
@@ -0,0 +1,15 @@
+package string.lc434;
+
+public class Lc434 {
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+}
