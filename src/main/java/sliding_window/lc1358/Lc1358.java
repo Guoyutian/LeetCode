b47169749143d6dd3bcbed7f72fe56919d435554
Guoyutian <929458988@qq.com>
2022-12-15 16:19:10 +0800

    1215

--- src/main/java/sliding_window/lc1358/Lc1358.java
+++ src/main/java/sliding_window/lc1358/Lc1358.java
@@ -2,14 +2,14 @@ package sliding_window.lc1358;
 
 public class Lc1358 {
     public int numberOfSubstrings(String s) {
-        int n = s.length(), ans=0,left=0,right=0;
+        int n = s.length(), ans = 0, left = 0, right = 0;
         char[] str = s.toCharArray();
         int[] count = new int[3]; // stores count of a,b,c;
-        while(right<n){
-            count[str[right]-'a']++;
-            while(left<right && count[0]>0 && count[1]>0 && count[2]>0){
-                ans += n-right; //count the substrings
-                count[str[left++]-'a']--; //move left boundry while decreasing the count
+        while (right < n) {
+            count[str[right] - 'a']++;
+            while (left < right && count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                ans += n - right; //count the substrings
+                count[str[left++] - 'a']--; //move left boundry while decreasing the count
             }
             right++;
         }
